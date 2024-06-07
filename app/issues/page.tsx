be72397252d22bfa0b53a726636e4d2ebd37b261
Pyxyll <pyxyllv@gmail.com>
2024-06-07 20:03:42 +0100

    Initial Commit, add issues, form sever and client validated.

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '@radix-ui/themes'
+
+const IssuesPage = () => {
+    return (
+        <div><Link href='/issues/new'><Button>New Issue</Button></Link></div>
+
+    )
+}
+
+export default IssuesPage
